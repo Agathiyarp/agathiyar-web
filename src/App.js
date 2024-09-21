92a00890417269cf9c154a4c398a522e8e40da7d
kvigneshece08 <kvigneshece08@gmail.com>
2024-09-21 16:07:07 +0530

    menu component creation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import MenuIntroduction from './components/menu/menu';
+import './App.css'
+import mainlogo from './images/mainlogo.png'
 
 function App() {
   const [username, setUsername] = useState('');
@@ -18,8 +24,25 @@ function App() {
   };
 
   return (
-    <div>
-      <input
+    <div class='main-container'>
+      <div class="image-container">
+        <img class='logo-image' src={mainlogo}/>
+        <div class='title'>
+          <h2 class='align-center m-30'>AGATHIYAR PYRAMID</h2>
+          <h2 class='align-center'>DHYANA ASHRAM</h2>
+          <h4 class='align-center m-0'>Mounam - Dhyanam - Gnanam</h4>
+        </div>
+      </div>
+      <MenuIntroduction/>
+      <div class='account-icon'>
+        <AccountCircleIcon/>
+      </div>
+      <div>
+      {/* <Stack direction="row" spacing={2}>
+        <Button>WORKSHOP LOGIN</Button>
+      </Stack> */}
+      </div>
+      {/* <input
         type="text"
         placeholder="Username"
         value={username}
@@ -33,7 +56,7 @@ function App() {
       />
       <button onClick={handleRegister}>Register</button>
       <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
+      <button onClick={handleLogout}>Logout</button> */}
     </div>
   );
 }
