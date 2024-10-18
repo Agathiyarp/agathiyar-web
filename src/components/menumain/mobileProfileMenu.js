fe00ac75e18b1f9463ebdc6499833906779f0fcd
kvigneshece08 <kvigneshece08@gmail.com>
2024-10-18 13:07:08 +0530

    mobile logout changes

--- src/components/menumain/mobileProfileMenu.js
+++ src/components/menumain/mobileProfileMenu.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { MenuItem, Avatar, Typography, Box, Divider } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import "./mobileProfileMenu.css";
+import mainlogo from "../../images/mainlogo.png";
 import { toast } from 'react-toastify';
-import './mobileProfileMenu.css';
-import mainlogo from "../../images/mainlogo.png"; 
 
 const MobileProfileMenu = ({ onLogout }) => {
   const navigate = useNavigate();
 
   const user = {
-    name: 'manoj',
-    memberId: 'AGP202400001',
+    name: "manoj",
+    memberId: "AGP202400001",
     visited: 10,
-    donartype: 'Premium',
-    profilePicture: mainlogo
+    donartype: "Premium",
+    profilePicture: mainlogo,
   };
 
   const handleLogout = async () => {
@@ -31,7 +31,9 @@ const MobileProfileMenu = ({ onLogout }) => {
       if (data) {
         toast.success("Logout successful!");
         sessionStorage.setItem('userDetails', '');
-        onLogout(); // Call the logout function passed from MenuBar
+        setTimeout(()=> {
+          navigate('/')
+        }, 3000);
       }
     } catch (err) {
       toast.error("Logout failed");
@@ -42,24 +44,38 @@ const MobileProfileMenu = ({ onLogout }) => {
     <>
       {user && (
         <MenuItem onClick={() => navigate("/profile")}>
-          <Avatar src={user.profilePicture} alt={user.name} sx={{ marginRight: "10px" }} />
+          <Avatar
+            src={user.profilePicture}
+            alt={user.name}
+            sx={{ marginRight: "10px" }}
+          />
           <Box>
             <Typography variant="subtitle1">{user.name}</Typography>
-            <Typography variant="body2" color="textSecondary">Member ID: {user.memberId}</Typography>
-            <Typography variant="body2" color="textSecondary">Visited: {user.visited} times</Typography>
-            <Typography variant="body2" color="textSecondary">Donor: {user.donartype}</Typography>
+            <Typography variant="body2" color="textSecondary">
+              Member ID: {user.memberId}
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+              Visited: {user.visited} times
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+              Donor: {user.donartype}
+            </Typography>
           </Box>
         </MenuItem>
       )}
       <Divider />
-      <MenuItem sx={{
-          color: '#38a169', // Change the text color
-          fontWeight: 'bold', // Make the font bold
+      <MenuItem
+        sx={{
+          color: "#38a169", // Change the text color
+          fontWeight: "bold", // Make the font bold
           "&:hover": {
-            backgroundColor: "#38a169", // Light red background on hover
+            backgroundColor: "#8bedb8", // Light red background on hover
           },
-          justifyContent: 'center'
-        }} onClick={handleLogout}>
+          justifyContent: "center",
+          margin: "20px",
+        }}
+        onClick={handleLogout}
+      >
         Logout
       </MenuItem>
       <Divider />
