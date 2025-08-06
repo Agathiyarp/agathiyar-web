c4e46ddf6aafaab282652b910813157544a239d4
vikasthu20 <vikasthu20@gmail.com>
2025-08-06 13:39:32 +0530

    images updated

--- src/components/GridViewVeg.js
+++ src/components/GridViewVeg.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ImageTextSection from './ImageTextSection';
+import ImageSection from './ImageSection';
 import vegImage from '../images/home/home4.jpg';
 
 const VegetarianSection = () => {
   return (
-    <ImageTextSection
+    <ImageSection
       imageSrc={vegImage}
       title="Being a Vegetarian"
       content="Vegetarianism is a conscious effort a deliberate effort to get out of the heaviness that keeps you tethered to the Earth so that you can fly. So that the flight from the alone to the alone becomes possible."
--- src/components/home/section/ImageSection.js
+++ src/components/home/section/ImageSection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './ImageTextSection.css';
+import './ImageSection.css';
 
 const ImageTextSection = ({ imageSrc, title, content }) => {
   return (
